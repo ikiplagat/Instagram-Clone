d3d57f7e1a65e138e30309121cdcf662f3a045ee
Kasparov <ianjkiplagat@gmail.com>
2021-05-20 22:09:48 +0300

    add profile page

--- Insta/views.py
+++ Insta/views.py
@@ -4,6 +4,13 @@ from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+# Feed
 @login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'index.html')
+
+# Profile page
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    # title = 'Profile'
+    return render(request, 'profile/profile.html')
