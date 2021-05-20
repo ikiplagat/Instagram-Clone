87858985a7da308021f1cc495058f3efbc0c0d97
Kasparov <ianjkiplagat@gmail.com>
2021-05-20 14:31:55 +0300

    add superuser

--- Insta/models.py
+++ Insta/models.py
@@ -2,14 +2,40 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
-class Profile(models.Model):
-  photo = CloudinaryField('photo')
-  bio = models.CharField(max_length =150)
 
+# Profile class.
+class Profile(models.Model):
+    photo = CloudinaryField('photo')
+    bio = models.CharField(max_length =150)
+  
+    def __str__(self):
+        return self.bio
+        
+    def save_profile(self):
+        self.save()
+          
+    def delete_profile(self):
+        self.delete()
+        
+    @classmethod
+    def update_profile(cls,id,bio):
+        cls.objects.filter(id=id).update(bio=bio)              
+
+
+# Image class.
 class Image(models.Model):
-  image = CloudinaryField('photo')
-  name = models.CharField(max_length =30)
-  caption = models.TextField(max_length =2200)
-  likes = models.IntegerField()
-  comments = models.CharField
-  profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    image = CloudinaryField('photo')
+    name = models.CharField(max_length =30)
+    caption = models.TextField(max_length =2200)
+    likes = models.IntegerField()
+    comments = models.CharField(max_length =2200)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+        
+    def save_image(self):
+        self.save()  
+          
+    def delete_image(self):
+        self.delete()          
--- Insta/tests.py
+++ Insta/tests.py
@@ -1,3 +1,63 @@
 from django.test import TestCase
+from .models import Profile, Image
 
 # Create your tests here.
+
+# Profile class tests
+class ProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.profile = Profile(photo = 'img', bio = 'I am Levlest')
+      
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+        
+    # Testing Save Method
+    def test_save_method(self):  
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+        
+    def tearDown(self):
+        Profile.objects.all().delete()
+      
+    def delete_profile(self):
+        self.delete() 
+        
+    def test_update_profile(self):
+        self.profile.save_profile()
+        self.profile.update_profile(self.profile.id,'I am Levlest')
+        update=Profile.objects.get(bio='I am Levlest')
+        self.assertEqual(update.bio,'I am Levlest')                      
+        
+        
+# Image class tests
+class ImageTestClass(TestCase):
+  # Set up method
+    def setUp(self):
+        self.profile= Profile(photo = 'img', bio = 'I am Levlest')
+        self.profile.save()
+        self.image = Image(image = 'img', name = 'Kasparov', caption = 'I am Levlest', likes = 0, comments = 'Wowz', profile = self.profile)
+      
+  # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))        
+
+    # Testing Save Method
+    def test_save_method(self):  
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+        
+    def tearDown(self):
+        Image.objects.all().delete() 
+        Profile.objects.all().delete()    
+      
+    def test_delete_image(self):
+        self.image.save_image()
+        images=Image.objects.all()
+        self.assertEqual(len(images),1)
+        self.image.delete_image()
+        del_images=Image.objects.all()
+        self.assertEqual(len(del_images),0)      
